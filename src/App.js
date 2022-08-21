1a149f761a638a780ed55637a7c3a4a4cd6197d9
DSmithMr <dsmith.mr@gmail.com>
2022-08-21 17:50:12 -0500

    error repo init

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+import { BandAdd } from "./componets/BandAdd";
+import { BandList } from "./componets/BandList";
+
+const connetSocketServer = () => {
+  const socket = io.connect("http://localhost:8080");
+  return socket;
+};
+
+function App() {
+  const [socket] = useState(connetSocketServer(), {
+    transports: ["websocket"],
+  });
+
+  const [online, setOnline] = useState(false);
+
+  useEffect(() => {
+    setOnline(socket.connected);
+  }, [socket]);
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      setOnline(true);
+    });
+  }, [socket]);
+
+  useEffect(() => {
+    socket.on("disconnect", () => {
+      setOnline(false);
+    });
+  }, [socket]);
+
+  return (
+    <div className="container">
+      <div className="alert">
+        <p>
+          Service status:
+          {online ? (
+            <span className="text-success">Online</span>
+          ) : (
+            <span className="text-danger">Offline</span>
+          )}
+        </p>
+      </div>
+      <h1>BandNames</h1>
+      <hr />
+      <div className="row">
+        <div className="col-8">
+          <BandList />
+        </div>
+        <div className="col-4">
+          <BandAdd />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componets/BandAdd.js
@@ -0,0 +1,10 @@
+export const BandAdd = () => {
+  return (
+    <>
+      <h3>Agregar banda</h3>
+      <form>
+        <input className="form-control" placeholder="Nuevo nombre" />
+      </form>
+    </>
+  );
+};

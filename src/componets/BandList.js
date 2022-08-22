28df90c07d41d3a456201bba4b0738086a17f79a
DSmithMr <dsmith.mr@gmail.com>
2022-08-22 13:48:10 -0500

    listar bandas entregadas por el back

--- src/componets/BandList.js
+++ src/componets/BandList.js
@@ -1,21 +1,31 @@
-export const BandList = () => {
+import { useEffect, useState } from "react";
+
+export const BandList = ({ data }) => {
+  const [bands, setBands] = useState(data);
+
+  useEffect(() => {
+    setBands(data);
+  }, [data]);
+
   const creatRows = () => {
-    return (
-      <tr>
-        <td>
-          <button className="btn btn-primary">+1</button>
-        </td>
-        <td>
-          <input className="form-control" />
-        </td>
-        <td>
-          <h3> 0 </h3>
-        </td>
-        <td>
-          <button className="btn btn-danger">Borrar</button>
-        </td>
-      </tr>
-    );
+    return bands.map((band) => {
+      return (
+        <tr key={band.id}>
+          <td>
+            <button className="btn btn-primary">+1</button>
+          </td>
+          <td>
+            <input className="form-control" value={band.name} />
+          </td>
+          <td>
+            <h3> 0 </h3>
+          </td>
+          <td>
+            <button className="btn btn-danger">Borrar</button>
+          </td>
+        </tr>
+      );
+    });
   };
 
   return (

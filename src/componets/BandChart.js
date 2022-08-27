ec751db62e144cd9cd56bffc6ef23a363cb11812
DSmithMr <dsmith.mr@gmail.com>
2022-08-27 14:16:57 -0500

    Added current-bands to BandChart

--- src/componets/BandChart.js
+++ src/componets/BandChart.js
@@ -1,17 +1,28 @@
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
 import Chart from "chart.js/auto";
+import { SocketContext } from "../context/SocketContext";
 
+let myChart;
 export const BandChart = () => {
+  const { socket } = useContext(SocketContext);
+
   useEffect(() => {
+    socket.on("current-bands", (bands) => {
+      crearGrafica(bands);
+    });
+  }, [socket]);
+
+  const crearGrafica = (bands = []) => {
     const ctx = document.getElementById("myChart");
-    const myChart = new Chart(ctx, {
+    if (typeof myChart !== "undefined") myChart.destroy();
+    myChart = new Chart(ctx, {
       type: "bar",
       data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: bands.map((band) => band.name),
         datasets: [
           {
             label: "# of Votes",
-            data: [12, 19, 3, 5, 2, 3],
+            data: bands.map((band) => band.votes),
             backgroundColor: [
               "rgba(255, 99, 132, 0.2)",
               "rgba(54, 162, 235, 0.2)",
@@ -33,14 +44,11 @@ export const BandChart = () => {
         ],
       },
       options: {
+        animation: false,
         indexAxis: "y",
       },
     });
-
-    return () => {
-      myChart.destroy();
-    };
-  }, []);
+  };
 
   return <canvas id="myChart"></canvas>;
 };

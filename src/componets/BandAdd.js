4f1d5a007d1ff1656624846190b2c70f2f4e1fe6
DSmithMr <dsmith.mr@gmail.com>
2022-08-27 09:54:43 -0500

    Added nueva-banda

--- src/componets/BandAdd.js
+++ src/componets/BandAdd.js
@@ -1,9 +1,26 @@
-export const BandAdd = () => {
+import { useState } from "react";
+
+export const BandAdd = ({ crearBanda }) => {
+  const [valor, setValor] = useState("");
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (valor.trim().length > 0) {
+      crearBanda(valor);
+      setValor("");
+    }
+  };
+
   return (
     <>
       <h3>Agregar banda</h3>
-      <form>
-        <input className="form-control" placeholder="Nuevo nombre" />
+      <form onSubmit={onSubmit}>
+        <input
+          className="form-control"
+          placeholder="Nuevo nombre"
+          value={valor}
+          onChange={(event) => setValor(event.target.value)}
+        />
       </form>
     </>
   );

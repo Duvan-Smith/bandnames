7a198e83c5e77216f0199e883871b90edf1eb8bd
DSmithMr <dsmith.mr@gmail.com>
2022-08-27 12:44:07 -0500

    Clean BandAdd

--- src/componets/BandAdd.js
+++ src/componets/BandAdd.js
@@ -1,10 +1,10 @@
-import { useState } from "react";
-import { useSocket } from "../hooks/useSocket";
+import { useContext, useState } from "react";
+import { SocketContext } from "../context/SocketContext";
 
 export const BandAdd = () => {
   const [valor, setValor] = useState("");
 
-  const { socket } = useSocket("http://localhost:8080");
+  const { socket } = useContext(SocketContext);
 
   const onSubmit = (event) => {
     event.preventDefault();
